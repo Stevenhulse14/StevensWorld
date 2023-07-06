7cf6b7a201d73caf44fbfdf80820fa9763257d08
steven <shotta3xd@gmail.com>
2023-07-05 22:52:30 -0400

    created tab component

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
-import { Avatar, Box, Typography, Divider } from "@mui/material";
+import { Avatar, Box, Typography, Divider, Grid } from "@mui/material";
+import TabsAbout from "./Tabs";
 
 interface ProfileProps {
   name: string;
@@ -9,7 +10,9 @@ interface ProfileProps {
 
 const Intro: FC<ProfileProps> = ({ name, bio, avatarUrl }) => {
   return (
-    <Box
+    <Grid
+      container
+      spacing={2}
       sx={{
         display: "flex",
         justifyContent: "space-evenly",
@@ -18,86 +21,97 @@ const Intro: FC<ProfileProps> = ({ name, bio, avatarUrl }) => {
         marginTop: "10px",
       }}
     >
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          flexDirection: "column",
-        }}
-      >
+      <Grid item xs={5}>
         <Box
+          className="container1"
           sx={{
             display: "flex",
-            justifyContent: "space-evenly",
+            justifyContent: "center",
             alignItems: "center",
-            flexDirection: "row",
+            flexDirection: "column",
           }}
         >
-          <Box sx={{ display: "flex", direction: "row", alignItems: "start" }}>
-            <Typography
-              color="green"
-              sx={{
-                fontSize: 8,
-                alignItems: "top",
-                textShadow: "#FC0 .5px 0 1px",
-                paddingBottom: "20px",
-              }}
-            >
-              Software Developer
-            </Typography>
-          </Box>
-          <Typography
-            variant="h5"
+          <Box
             sx={{
-              fontSize: "40px",
-              color: "#FFFFFF",
-              fontWeight: "900",
-              fontFamily: "Roboto",
-              marginRight: "50px",
-              textShadow:
-                "0 1px 0 #ccc, 0 2px 0 #c9c9c9,0 3px 0 #bbb, 0 4px 0 #b9b9b9, 0 5px 0 #aaa, 0 6px 1px rgba(0,0,0,.1), 0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.3), 0 3px 5px rgba(0,0,0,.2), 0 5px 10px rgba(0,0,0,.25), 0 10px 10px rgba(0,0,0,.2), 0 20px 20px rgba(0,0,0,.15);",
+              display: "flex",
+              justifyContent: "space-evenly",
+              alignItems: "center",
+              flexDirection: "row",
+              marginBottom: "50px",
             }}
           >
-            {name}
+            <Typography
+              variant="h5"
+              sx={{
+                fontSize: "24px",
+                color: "#FFFFFF",
+                fontWeight: "900",
+                fontFamily: "sans-serif",
+                marginRight: "50px",
+                textShadow:
+                  "0 1px 0 #ccc, 0 2px 0 #c9c9c9,0 3px 0 #bbb, 0 4px 0 #b9b9b9, 0 5px 0 #aaa, 0 6px 1px rgba(0,0,0,.1), 0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.3), 0 3px 5px rgba(0,0,0,.2), 0 5px 10px rgba(0,0,0,.25), 0 10px 10px rgba(0,0,0,.2), 0 20px 20px rgba(0,0,0,.15);",
+              }}
+            >
+              {name}
+            </Typography>
+            <Box
+              sx={{ display: "flex", direction: "row", alignItems: "start" }}
+            >
+              <Typography
+                color="green"
+                sx={{
+                  fontSize: 8,
+                  alignItems: "top",
+                  textShadow: "#FC0 .5px 0 1px",
+                  paddingBottom: "20px",
+                }}
+              >
+                Software Developer
+              </Typography>
+            </Box>
+          </Box>
+          <Avatar
+            src={avatarUrl}
+            alt="Profile Picture"
+            sx={{ width: 200, height: 200 }}
+          />
+          <Divider sx={{ my: 2 }} />
+          <Box
+            sx={{ display: "flex", flexDirection: "row", alignItems: "center" }}
+          >
+            <Typography variant="h6" sx={{ fontSize: "10px" }}>
+              I am a Fullstack Developer :
+            </Typography>
+            <Typography sx={{ fontSize: "16px" }}> 🤓</Typography>
+          </Box>
+          <Typography variant="h6" sx={{ fontSize: "10px" }}>
+            United States, Brooklyn, NY
+          </Typography>
+          <Typography variant="h6" sx={{ fontSize: "10px" }}>
+            Email : Stevenhulse1993@gmail.com | Phone : 347-951-1234
           </Typography>
         </Box>
-        <Avatar
-          src={avatarUrl}
-          alt="Profile Picture"
-          sx={{ width: 200, height: 200 }}
-        />
-        <Typography variant="h6">I am a full stack developer</Typography>
-      </Box>
+      </Grid>
       <Divider orientation="vertical" variant="middle" flexItem />
-
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          flexDirection: "column",
-        }}
-      >
-        <Typography variant="h5">About Me</Typography>
+      <Grid item xs={6}>
+        <Box
+          className="container2"
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "flex-start",
+            justifyContent: "center",
+            height: "100%",
+          }}
+        >
+          <TabsAbout />
+          {/* <Typography variant="h5">About Me</Typography>
         <Box sx={{ display: "flex", justifyContent: "center" }}>
           <Typography variant="body1">{bio}</Typography>
+        </Box> */}
         </Box>
-      </Box>
-
-      <Divider orientation="vertical" variant="middle" flexItem />
-
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          flexDirection: "column",
-        }}
-      >
-        <Typography variant="h5">Contact Info</Typography>
-      </Box>
-    </Box>
+      </Grid>
+    </Grid>
   );
 };
 
---
+++ src/components/Tabs.tsx
@@ -0,0 +1,69 @@
+import { Tab, Tabs, Box, Typography } from "@mui/material";
+import { SetStateAction, useState } from "react";
+
+function TabsAbout() {
+  const [tabIndex, setTabIndex] = useState(0);
+  const handleTabChange = (event: any, newTabIndex: SetStateAction<number>) => {
+    //console.log(newTabIndex);
+    setTabIndex(newTabIndex);
+  };
+  //console.log(tabIndex);
+  return (
+    <Box
+      sx={{
+        color: "whitesmoke",
+        paddingBottom: "20px",
+        maxWidth: "100%",
+      }}
+    >
+      <Tabs
+        value={tabIndex}
+        onChange={handleTabChange}
+        sx={{ display: "flex", justifyContent: "center" }}
+        orientation="vertical"
+      >
+        <Tab label="About" sx={{ color: "whitesmoke" }} />
+        <Tab label="Projects" sx={{ color: "whitesmoke" }} />
+        <Tab label="Contact" sx={{ color: "whitesmoke" }} />
+      </Tabs>
+      <Box sx={{ margin: 2, height: "90%", maxWidth: "50%" }}>
+        {tabIndex === 0 && (
+          <Box textAlign="center" alignContent="center" height="100%">
+            <Typography alignContent="center">
+              I am a Software Developer Army Veteran that stationed in NYC. I am
+              fun outgoing and free spirited. Some activities that I enjoy on my
+              free time are listening to music, coding, playing video games, and
+              going on little nyc adventures. I am a Software Developer Army
+              Veteran that stationed in NYC. I am fun outgoing and free
+              spirited. Some activities that I enjoy on my free time are
+              listening to music, coding, playing video games, and going on
+              little nyc adventures.
+            </Typography>
+          </Box>
+        )}
+        {tabIndex === 1 && (
+          <Box height="100%">
+            <Typography>
+              I am a Software Developer Army Veteran that stationed in NYC. I am
+              fun outgoing and free spirited. Some activities that I enjoy on my
+              free time are listening to music, coding, playing video games, and
+              going on little nyc adventures.
+            </Typography>
+          </Box>
+        )}
+        {tabIndex === 2 && (
+          <Box height="100%">
+            <Typography>
+              I am a Software Developer Army Veteran that stationed in NYC. I am
+              fun outgoing and free spirited. Some activities that I enjoy on my
+              free time are listening to music, coding, playing video games, and
+              going on little nyc adventures.
+            </Typography>
+          </Box>
+        )}
+      </Box>
+    </Box>
+  );
+}
+
+export default TabsAbout;
